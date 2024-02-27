fe3bf4d858aa582b44b4a7ce07702a0bfccdb120
GustavSoederberg <guso0001@stud.kea.dk>
2024-02-27 10:47:14 +0100

    Initial commit

---
+++ src/main/java/com/example/githubactionsdemo/GithubActionsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsDemoApplication.class, args);
+    }
+
+}
